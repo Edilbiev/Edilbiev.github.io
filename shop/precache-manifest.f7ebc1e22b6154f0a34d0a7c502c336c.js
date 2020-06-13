375998315968fae4d62a0be2463c3e635e5efaf9
Yusup Edilbiev <chohban@bk.ru>
2020-06-13 09:20:21 +0300

    shop folder added

---
+++ shop/precache-manifest.f7ebc1e22b6154f0a34d0a7c502c336c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3099b4346a0a017e48257da1d705b40d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d47ea15cd72e6d8fcd69",
+    "url": "./static/js/2.db5b2939.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.db5b2939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843ebba9f56c85d64f9a",
+    "url": "./static/js/main.1586fe70.chunk.js"
+  },
+  {
+    "revision": "235d6d02cf59e44a12da",
+    "url": "./static/js/runtime-main.536cc3fe.js"
+  },
+  {
+    "revision": "4e6c5b599de59e4850083a1aeb081a66",
+    "url": "./static/media/full-word.4e6c5b59.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "./static/media/shopping-cart-solid.8bf1af37.svg"
+  }
+]);
