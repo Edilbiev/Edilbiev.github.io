33f43c3aaf6321d93b1137e21d57017715cf55d3
Yusup Edilbiev <chohban@bk.ru>
2020-06-21 00:06:19 +0300

    accordion folder added

---
+++ accordion/precache-manifest.b8a7f732a6bca87c8141059538a8c78b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb2a56901d746860a4e0021a2273844",
+    "url": "./index.html"
+  },
+  {
+    "revision": "65d42470bafa90462cda",
+    "url": "./static/js/2.58348635.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.58348635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cfa1aa4bf66d4488a30",
+    "url": "./static/js/main.7452bec4.chunk.js"
+  },
+  {
+    "revision": "da46bbae465c6e9a2815",
+    "url": "./static/js/runtime-main.e10754f8.js"
+  }
+]);
