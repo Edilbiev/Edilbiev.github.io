7fc29ec6ef6a5008955946b91c2e251e680aef9a
Yusup Edilbiev <chohban@bk.ru>
2020-06-21 15:38:19 +0300

    sapper folder added

---
+++ sapper/precache-manifest.8f750b9c078ee9e79a195abe525de398.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02cc8ca4fd5498f053d40fc588ca1655",
+    "url": "./index.html"
+  },
+  {
+    "revision": "84a30aad001f6ec8cb4e",
+    "url": "./static/js/2.adf3ed00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.adf3ed00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566e79c4eeece7d5b21a",
+    "url": "./static/js/main.32eb7a6f.chunk.js"
+  },
+  {
+    "revision": "8d8fc2d38899a01a5f94",
+    "url": "./static/js/runtime-main.2f3bb1c2.js"
+  }
+]);
