3a324af3fe10d75a9eb39aacc4cc85f4ab417110
Yusup Edilbiev <chohban@bk.ru>
2020-06-21 18:48:27 +0300

    sidemenu folder added

---
+++ side-menu/precache-manifest.739548f0b1d3ea4d5c39456fc964601b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beba45d4688d886b1be4830d9a9fc8fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cae3e819dc4bf6f9804f",
+    "url": "./static/css/2.62afb475.chunk.css"
+  },
+  {
+    "revision": "cae3e819dc4bf6f9804f",
+    "url": "./static/js/2.f0fd8c34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f0fd8c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba68c5fd15473a741fea",
+    "url": "./static/js/main.3b738d34.chunk.js"
+  },
+  {
+    "revision": "ef1a81695c1534f9da33",
+    "url": "./static/js/runtime-main.be46cdb1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
