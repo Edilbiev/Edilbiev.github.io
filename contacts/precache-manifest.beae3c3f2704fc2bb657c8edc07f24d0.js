530d886d98e2cb7692fe1916b95ba208c379d900
Yusup <chohban@bk.ru>
2020-10-12 13:26:23 +0300

    contacts folder updated

---
+++ contacts/precache-manifest.beae3c3f2704fc2bb657c8edc07f24d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb0dcac246f7350b2eccc4c865fe5af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28f07d59b5db15f2181d",
+    "url": "./static/js/2.66b8e611.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.66b8e611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22ef166c39a757e309d",
+    "url": "./static/js/main.86678aec.chunk.js"
+  },
+  {
+    "revision": "f7a448b20835d30b1ca9",
+    "url": "./static/js/runtime-main.4771f975.js"
+  }
+]);
--- contacts/service-worker.js
+++ contacts/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/contacts/precache-manifest.7973ecf890411ec97a52d5e46875675a.js"
+  "./precache-manifest.beae3c3f2704fc2bb657c8edc07f24d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contacts/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
