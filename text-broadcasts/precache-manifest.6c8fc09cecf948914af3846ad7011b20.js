80f4f01ebcb61ab5c0f3bcf4d4807a8c15573394
Yusup <chohban@bk.ru>
2020-10-19 21:36:09 +0300

    text-broadcats folder added

---
+++ text-broadcasts/precache-manifest.6c8fc09cecf948914af3846ad7011b20.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cca68631851c5f693aadb26eca3e4df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f3167b9f9ccdf1a41c88",
+    "url": "./static/css/2.2a37a751.chunk.css"
+  },
+  {
+    "revision": "f054dc1619d9d2cddea4",
+    "url": "./static/css/main.6ae196cc.chunk.css"
+  },
+  {
+    "revision": "f3167b9f9ccdf1a41c88",
+    "url": "./static/js/2.cceb1601.chunk.js"
+  },
+  {
+    "revision": "61876c10feb06c29abcb1f25684702ec",
+    "url": "./static/js/2.cceb1601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f054dc1619d9d2cddea4",
+    "url": "./static/js/main.d4dfff10.chunk.js"
+  },
+  {
+    "revision": "9815e377db2e4d89238a",
+    "url": "./static/js/runtime-main.f1747c35.js"
+  },
+  {
+    "revision": "9212ce7d8d031b1140ac2b2c769866eb",
+    "url": "./static/media/burger.9212ce7d.svg"
+  },
+  {
+    "revision": "4d007d82a7b331eae977fa15ac00a572",
+    "url": "./static/media/data_usage-24px.4d007d82.svg"
+  },
+  {
+    "revision": "d644ed957e2662b1d22adb890310e64f",
+    "url": "./static/media/more_horiz-24px.d644ed95.svg"
+  }
+]);
