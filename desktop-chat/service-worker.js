ece9d2b4e970781fae93a0810853a138f3ad3ffc
Yusup <chohban@bk.ru>
2020-11-10 07:01:27 +0300

    desktop-chat folder updated

--- desktop-chat/service-worker.js
+++ desktop-chat/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/desktop-chat/precache-manifest.03dc42b5208ea016547b6c9f08c1a247.js"
+  "./precache-manifest.5ce5ba8e67796db204fc970f5d002ab3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/desktop-chat/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
